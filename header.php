a17f854e8e8e8d7c2e81a1d12b39494225a1d50a
ragnaros123 <68516384+ragnaros123@users.noreply.github.com>
2022-10-25 13:41:28 +0100

    test1

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 
 <!-- Navbars -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light mx-2">
-  <a class="navbar-brand" href="#">Auction111 <!--CHANGEME!--></a>
+  <a class="navbar-brand" href="#">Auction1111 <!--CHANGEME!--></a>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
     
