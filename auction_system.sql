7933c02c29e0ec8fe177ad2dce000efb5fce0690
KennyDAI2000 <107610120+KennyDAI2000@users.noreply.github.com>
2022-11-07 14:32:09 +0000

    Debug place_bid.php &Update .sql
    Debugged passing value to bidhistory when buyer bids by prohibiting it
    Added bidID as PK in sql

---
+++ auction_system.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1:3306
+-- 生成日期： 2022-11-07 14:29:03
+-- 服务器版本： 5.7.36
+-- PHP 版本： 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `auction_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `auctions`
+--
+
+DROP TABLE IF EXISTS `auctions`;
+CREATE TABLE IF NOT EXISTS `auctions` (
+  `itemID` int(11) NOT NULL AUTO_INCREMENT,
+  `itemName` varchar(50) DEFAULT NULL,
+  `itemDescription` varchar(200) DEFAULT NULL,
+  `category` varchar(30) DEFAULT NULL,
+  `startingPrice` int(11) DEFAULT NULL,
+  `reservePrice` int(11) DEFAULT NULL,
+  `endDate` varchar(20) DEFAULT NULL,
+  `sellerID` int(11) DEFAULT NULL,
+  `highestBid` int(11) DEFAULT NULL,
+  `auctionStatus` int(11) DEFAULT NULL,
+  `buyerID` int(30) NOT NULL,
+  PRIMARY KEY (`itemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `auctions`
+--
+
+INSERT INTO `auctions` (`itemID`, `itemName`, `itemDescription`, `category`, `startingPrice`, `reservePrice`, `endDate`, `sellerID`, `highestBid`, `auctionStatus`, `buyerID`) VALUES
+(18, 'AJ1', 'Fancy shows', '3', 100, 200, '2022-11-09T12:44', 2, 133, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `bidhistory`
+--
+
+DROP TABLE IF EXISTS `bidhistory`;
+CREATE TABLE IF NOT EXISTS `bidhistory` (
+  `bidID` int(30) NOT NULL,
+  `buyerID` int(11) DEFAULT NULL,
+  `itemID` int(11) DEFAULT NULL,
+  `bidPrice` int(11) DEFAULT NULL,
+  `bidTime` time DEFAULT NULL,
+  PRIMARY KEY (`bidID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `account_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `password`, `email`, `account_type`) VALUES
+(2, 'e44daa2696689a3ec249cf684bc0670c6935d1bb', 'chengkai.dai.22@ucl.ac.uk', 1),
+(1, 'e44daa2696689a3ec249cf684bc0670c6935d1bb', '1823963114@qq.com', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- place_bid.php
+++ place_bid.php
@@ -1,42 +1,53 @@
 <?php
+include_once('database.php');
+session_start();
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
 // Notify user of success/failure and redirect/give navigation options.
 $item_id = $_POST['item_id']; //itemID is not null
-session_start();
-if (!isset($_SESSION)){            
-    echo ("Please login berfore biding");
+
+if ($_SESSION['logged_in']==false || $_SESSION['account_type'] != 'buyer'){            
+    echo ("Please login as buyer berfore biding");
     header("refresh:3;url=listing.php?item_id=$item_id");
     return;                  //To confirm whether users have login
-}
+}else{
     //TODO????: WHAT IF THE AUCTION END AT THE TIME TYPING THE BID PRICE?
-$_POST['bidInput'];
-$bidInput = $_POST ['bidInput'];
-if (empty($bidInput)){
-    echo ("You didn't enter any number");
-    header("refresh:3;url=listing.php?item_id=$item_id");
-    return;
-}
-include_once('database.php');
+  $_POST['bidInput'];
+  $bidInput = $_POST ['bidInput'];
+
   $sql = "SELECT highestBid FROM auctions WHERE itemID = $item_id";
   $result = mysqli_query($connection,$sql);
   $row = mysqli_fetch_row($result);
   $highestBid = $row[0];
-  
-if ($bidInput <= $highestBid){
+
+if (empty($bidInput)){
+    echo ("You didn't enter any number");
+    header("refresh:3;url=listing.php?item_id=$item_id");
+    return;
+}elseif($bidInput <= $highestBid){
     echo ("You can not enter a bid price that less than the current highest bid price");
     // header("refresh:3;url=listing.php?item_id=$item_id");
-}
-
-else{
+}else{
     $SESSION_id = $_SESSION['id'];
     $bidSubmitTime = date_format(new DateTime(), 'Y-m-d H:i:s');
     $update_sql = "UPDATE auctions SET highestBid = $bidInput, buyerID = $SESSION_id where itemID = $item_id";
     $update_result = mysqli_query($connection,$update_sql);
-    $insert_sql = "INSERT INTO bidhistory(`buyerID`,`itemID`,`bidPrice`,`bidTime`) VALUES ('$SESSION_id','$item_id','$bidInput','$bidSubmitTime')";
+
+    $bid_id = "SELECT * FROM bidhistory WHERE bidID=(SELECT MAX(bidID) FROM bidhistory)";
+    $result_id = mysqli_query($connection,$bid_id);
+    $row_id = mysqli_fetch_array($result_id);
+
+if($row_id){
+    $bid_id_iput=$row_id['bidID']+1;
+}else{
+    //For first user registration
+    $bid_id_iput=1;
+}
+    $insert_sql = "INSERT INTO bidhistory(`bidID`,`buyerID`,`itemID`,`bidPrice`,`bidTime`) VALUES ($bid_id_iput,'$SESSION_id','$item_id','$bidInput','$bidSubmitTime')";
     $insert_result = mysqli_query($connection,$insert_sql);
     echo ("Congratulations! You have sucessfully submit your bid!");
     // header("refresh:3;url=listing.php?item_id=$item_id");
 }
 header("refresh:3;url=listing.php?item_id=$item_id");
+}
 ?>
