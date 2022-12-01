44b8f4fff1204838a46ea084c8ee102bd160829d
PaperWings2019 <bill.liu1999@gmail.com>
2022-12-01 20:20:27 +0000

    Update watchlist.php

--- watchlist.php
+++ watchlist.php
@@ -34,7 +34,7 @@ if (mysqli_query($connection, $query)){
     }
 
     $infos = mysqli_fetch_row($result);
-    var_dump($infos);
+    // var_dump($infos);
 
     $bids_number_query = "SELECT count(*) FROM `bidhistory` WHERE itemID = $item[0]";
     if(mysqli_query($connection, $new_query)){
